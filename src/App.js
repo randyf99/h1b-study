67ab5cb995ba3b660e5c299f50bdf5914b075088
Randy Ferrer <randfer@gmail.com>
2017-08-07 16:37:27 -0400

    Added choropleth map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import _ from 'lodash';
 import Preloader from './components/Preloader';
 import { loadAllData } from './DataHandling';
+import CountyMap from './components/CountyMap';
 
 class App extends Component {
   state = {
@@ -15,16 +16,48 @@ class App extends Component {
     loadAllData(data => this.setState(data));
   }
 
+  countyValue(county, techSalariesMap) {
+    const medianHousehold = this.state.medianIncomes[county.id];
+    const salaries = techSalariesMap[county.name];
+
+    if (!medianHousehold || !salaries) {
+      return null;
+    }
+
+    const median = d3.median(salaries, d => d.base_salary);
+
+    return {
+      countyID: county.id,
+      value: median - medianHousehold.medianIncome
+    };
+  }
+
   render() {
     if (this.state.techSalaries.length < 1) {
       return <Preloader />;
     }
 
+    const filteredSalaries = this.state.techSalaries;
+    const filteredSalariesMap = _.groupBy(filteredSalaries, 'countyID');
+    const countyValues = this.state.countyNames
+      .map(county => this.countyValue(county, filteredSalariesMap))
+      .filter(d => !_.isNull(d));
+
+    let zoom = null;
     return (
       <div className="App container">
-        <h1>
-          Loaded {this.state.techSalaries.length} salaries
-        </h1>
+        <svg width="1100" height="500">
+          <CountyMap
+            usTopoJson={this.state.usTopoJson}
+            USstateNames={this.state.USstateNames}
+            values={countyValues}
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            zoom={zoom}
+          />
+        </svg>
       </div>
     );
   }
